f6f5ed7f69631e23e1cd594500482f620df96fb1
vinycxuz <vinyby@gmail.com>
2024-12-02 17:07:10 -0300

    (feat): clean some files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-           <code>src/App.js</code> and save to reload.
+    
         </p>
       </header>
     </div>
