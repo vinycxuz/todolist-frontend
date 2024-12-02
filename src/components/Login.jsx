0ae46c8b0918414e2f333f8a17d3fdb8c2494e0c
vinycxuz <vinyby@gmail.com>
2024-12-02 19:20:28 -0300

    (1.0)(fix): clean some information in login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -35,12 +35,8 @@ const Login = () => {
   return (
     <div class="flex flex-col w-full md:w-1/2 xl:w-2/5 2xl:w-2/5 3xl:w-1/3 mx-auto p-8 md:p-10 2xl:p-12 3xl:p-14 bg-[#ffffff] rounded-2xl shadow-xl">
       <div class="flex flex-row gap-3 pb-4">
-        <div>
-          <img src="/favicon.svg" alt="Logo" width="50" />
-        </div>
-        <h1 class="text-3xl font-bold text-[#4B5563] my-auto">Your Company</h1>
+        <h1 class="text-3xl font-bold text-[#4B5563] my-auto">ToDo App</h1>
       </div>
-      <div class="text-sm font-light text-[#6B7280] pb-8">Login to your account on Your Company.</div>
       <form class="flex flex-col" onSubmit={handleSubmit}>
         <div class="pb-2">
           <label htmlFor="email" class="block mb-2 text-sm font-medium text-[#111827]">Email</label>
@@ -69,7 +65,7 @@ const Login = () => {
           </div>
         </div>
         <button type="submit" class="w-full text-[#FFFFFF] bg-[#4F46E5] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-6">Login</button>
-        <div class="text-sm font-light text-[#6B7280]">Don't have an account yet? <a href="#" class="font-medium text-[#4F46E5] hover:underline">Sign Up</a></div>
+        <div class="text-sm font-light text-[#6B7280]">Don't have an account yet? <a href="/register" class="font-medium text-[#4F46E5] hover:underline">Sign Up</a></div>
       </form>
     </div>
   );
